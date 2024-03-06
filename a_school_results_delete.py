a52d85d4660a5c1a489709335e504b3bb4acbf80
Daniel-Chacha <danmwita355@gmail.com>
2024-03-06 18:05:43 +0300

    passed the dataframe to a file using a function

--- a_school_results_delete.py
+++ a_school_results_delete.py
@@ -19,8 +19,23 @@ def studentresult():
         print(df.loc[[z]])
 
     #print('Enter yes to delete or no to exit')
-    a=input("Enter 'yes' to delete or 'no' to exit: ")
-    option=a.lower()
+    def confirm_deletion():
+        a=input("Enter 'yes' to delete or 'no' to exit: ")
+        option=a.lower()
+        #print(matching_row)
+        if option =='yes':
+            new_df=df.drop(matching_row.index)
+            print(new_df)
+            from a_school_results_entry_system_replica import excel_file
+            l=excel_file(new_df)
+            #new_df.to_excel('a-results2.xlsx',index=False)
+        elif option =='no':
+            print('EXITING WITHOUT DELETING')
+            studentresult()
+        else:
+            print("Invalid Input: Please enter 'yes' or 'no' ")
+            confirm_deletion()  
+    confirm_deletion()
     
 
 studentresult()
